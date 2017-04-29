924c13c9eb5c9e93c1824ff8f8f915e3d6867184
Dmitry Andrienko <d.andrienko@live.com>
2017-04-29 14:53:20 +0300

    initial test app

---
+++ deploy-envs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=netcoreapps-welthperk
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=us-west-2
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=ec2-54-71-183-9.us-west-2.compute.amazonaws.com
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
