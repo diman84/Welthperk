08ba3c5d1f4d78e3b5901edbb6bcc50b31df9d58
d.andrienko <d.andrienko@live.com>
2017-05-11 19:01:08 +0300

    added internal host for api

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -9,6 +9,7 @@ export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=ec2-54-71-183-9.us-west-2.compute.amazonaws.com
+export AWS_INTERNAL_HOST=ip-172-31-2-234.us-west-2.compute.internal
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
